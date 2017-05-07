84abcf246a16392e63e26e544dd873abf65d95cb
Michał Pawłowicz <michal.pawlowicz@yahoo.com>
2017-05-07 14:35:38 +0200

    Create setup.py and automaticall instalation

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="Approximate complexity calculator",
+    version='0.1',
+    description="Calculate complexity",
+    author='Michał Pawłowicz',
+    license='MIT'
+)
